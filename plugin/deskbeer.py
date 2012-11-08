ded5458e95ac52cf4b3ab9d132b1be80e0a2d9dd
Jose Maria Ruiz <jose.ruiz@hogarthww.com>
2012-11-08 14:03:15 +0000

    Add tests and thursday grey freebeers

--- plugin/deskbeer.py
+++ plugin/deskbeer.py
@@ -1,6 +1,7 @@
 import datetime
 
-from . import helpers
+import helpers
+import unittest
 
 def format_time_left(now, deskbeer_time):
     tdiff = now - deskbeer_time
@@ -17,28 +18,119 @@ def format_time_left(now, deskbeer_time):
         return "about 1 hour"
     return "about {} hours".format(round(diff_min / 60.0))
 
+
+MESSAGES = {
+    'monday': ["Calm down drunkard, it is only Monday afterall!"],
+    'tuesday': ["Tuesday isn't deskbeer day, " \
+               "but definitely feel less bad going to the pub after work."],
+    'wednesday': ["If you're a geek it's games night, " \
+               "if you're not, have fun with the hangover tomorrow. " \
+               "Deskbeer not required."],
+    'thursday': [
+        "Run, you fool! Grey is paying for the beers downstairs!",
+        "Grey's FreeBEERs will be there in just {0}, don't despair yet!"],
+    'friday': ["Deskbeer needs to be aquired in {}",
+               "Deskbeer should be on it's way or in your belly!"],
+    'weekend': ["Why are you here on the weekend?"]
+}
+
+
+def decide_deskbeer_message(now):
+    """Returns the right message depending on the value of now."""
+
+    msg = None
+
+    if now.isoweekday() == 1:
+        msg = MESSAGES['monday'][0]
+    elif now.isoweekday() == 2:
+        msg =  MESSAGES['tuesday'][0]
+    elif now.isoweekday() == 3:
+        msg =  MESSAGES['wednesday'][0]
+    elif now.isoweekday() == 4:
+        freebeer_time = datetime.datetime(now.year, now.month, now.day, 18)
+        if now >= freebeer_time:
+            msg = MESSAGES['thursday'][0]
+        else:
+            time_left = format_time_left(now, freebeer_time)
+            msg = MESSAGES['thursday'][1]
+
+    elif now.isoweekday() == 5:
+        deskbeer_time = datetime.datetime(now.year, now.month, now.day, 17)
+        if now >= deskbeer_time:
+            msg = MESSAGES['friday'][1]
+        else:
+            time_left = format_time_left(now, deskbeer_time)
+            msg = MESSAGES['friday'][0].format(time_left)
+
+    else:
+        msg = MESSAGES['weekend'][0]
+
+    return msg
+
+
 def deskbeer_message():
     "every friday, 5pm"
     now = datetime.datetime.now()
-    if now.isoweekday() == 1:
-        return "Calm down drunkard, it is only Monday afterall!"
-    if now.isoweekday() == 2:
-        return "Tuesday isn't deskbeer day, " \
-               "but definitely feel less bad going to the pub after work."
-    if now.isoweekday() == 3:
-        return "We're getting there, only 2 days to go!"
-    if now.isoweekday() == 4:
-        return "If you're a geek it's games night, " \
-               "if you're not, have fun with the hangover tomorrow. " \
-               "Deskbeer not required."
-    if now.isoweekday() == 5:
-        deskbeer_time = datetime.datetime(now.year, now.month, now.day, 17)
-        if now > deskbeer_time:
-            return "Deskbeer should be on it's way or in your belly!"
-        time_left = format_time_left(now, deskbeer_time)
-        return "Deskbeer needs to be aquired in {}".format(time_left)
-    return "Why are you here on the weekend?"
+    return decide_deskbeer_message(now)
 
 @helpers.commands('deskbeer')
 def deskbeer(bot, conn, event):
     conn.privmsg(event.target(), deskbeer_message())
+
+
+
+class TestDeskbeer(unittest.TestCase):
+
+    def test_monday(self):
+        monday = datetime.datetime(2011, 9, 5, 10)
+        self.assertEqual(decide_deskbeer_message(monday), MESSAGES['monday'][0])
+
+    def test_tuesday(self):
+        tuesday = datetime.datetime(2011, 9, 6, 10)
+        self.assertEqual(decide_deskbeer_message(tuesday), MESSAGES['tuesday'][0])
+
+    def test_wednesday(self):
+        wednesday = datetime.datetime(2011, 9, 7, 10)
+        self.assertEqual(decide_deskbeer_message(wednesday), MESSAGES['wednesday'][0])
+
+    def test_thursday(self):
+        thursday = datetime.datetime(2011, 9, 8, 10)
+        self.assertEqual(decide_deskbeer_message(thursday), MESSAGES['thursday'][1])
+
+        thursday = datetime.datetime(2011, 9, 8, 18)
+        self.assertEqual(decide_deskbeer_message(thursday), MESSAGES['thursday'][0])
+
+        thursday = datetime.datetime(2011, 9, 8, 18, 1)
+        self.assertEqual(decide_deskbeer_message(thursday), MESSAGES['thursday'][0])
+
+
+
+
+
+
+    def test_friday(self):
+        friday = datetime.datetime(2011, 9, 9, 10)
+        self.assertEqual(decide_deskbeer_message(friday), MESSAGES['friday'][0].format('about 7.0 hours'))
+
+        friday = datetime.datetime(2011, 9, 9, 16, 59)
+        self.assertEqual(decide_deskbeer_message(friday), MESSAGES['friday'][0].format('1 minutes'))
+
+        friday = datetime.datetime(2011, 9, 9, 17, 00)
+        self.assertEqual(decide_deskbeer_message(friday), MESSAGES['friday'][1])
+
+        friday = datetime.datetime(2011, 9, 9, 17, 01)
+        self.assertEqual(decide_deskbeer_message(friday), MESSAGES['friday'][1])
+
+
+
+
+    def test_weekend(self):
+        weekend = datetime.datetime(2011, 9, 10)
+        self.assertEqual(decide_deskbeer_message(weekend), MESSAGES['weekend'][0])
+        weekend = datetime.datetime(2011, 9, 11)
+        self.assertEqual(decide_deskbeer_message(weekend), MESSAGES['weekend'][0])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
